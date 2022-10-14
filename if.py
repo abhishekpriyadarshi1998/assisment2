6d9c680684f0d0913717701dcdd23e3c52d42d89
abhishekpriyadarshi1998 <115804419+abhishekpriyadarshi1998@users.noreply.github.com>
2022-10-14 20:08:43 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+x=input("Enter Today's Day")
+if(x=="Sunday"):
+    print("Then we go out for game")
+elif(x=="Monday"):
+    print("I gp for Movie")
+elif(x=="Tuesday"):
+    print("I will go to temple")    
+elif(x=="Wednesday"):
+    print("I will go to the Library")
+print(x)    
